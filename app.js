c34a7b1647b5af971be6ebc0011be602a1e2b1f3
Keerthi Vardhan <keerthivardhan1029@gmail.com>
2025-07-22 16:01:36 +0530

    Changed app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== "production") {
     console.log("✅ Loaded .env for development");
 }
 
+
 // const requiredEnvVars = ["ATLASDB_URL", "SECRET", "CLOUD_NAME","CLOUD_API_KEY","CLOUD_API_SECRET"];
 // requiredEnvVars.forEach((key) => {
 //     if (!process.env[key]) {
