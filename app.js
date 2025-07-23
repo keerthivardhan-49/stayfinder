ffa173e9a26e709bd26d4dec7a8d757b170bc26b
Keerthi Vardhan <keerthivardhan1029@gmail.com>
2025-07-23 12:15:43 +0530

    Changed app.js

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ Start Server
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => {
     console.log(`✅ Server is listening on port ${port}`);
 });
